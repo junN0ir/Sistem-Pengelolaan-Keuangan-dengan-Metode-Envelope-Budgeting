f35eacc52baa6b97885c446b8a8639efb4fa5154
Junnior <junniormarcellino20@gmail.com>
2024-05-12 16:18:47 +0800

    Create Pemasukan.java

---
+++ src/Model/Pemasukan.java
@@ -0,0 +1,38 @@
+package sistemkeuangan;
+
+import java.util.Date;
+
+public class Pemasukan extends Anggaran {
+    private double totalPemasukan;
+    private String sumber;
+
+    public Pemasukan(double totalAnggaran, Date tanggal, String deskripsi, double totalPemasukan, String sumber) {
+        super(totalAnggaran, tanggal, deskripsi);
+        this.totalPemasukan = totalPemasukan;
+        this.sumber = sumber;
+    }
+
+    // getter methods
+    public double getTotalPemasukan() {
+        return this.totalPemasukan;
+    }
+
+    public String getSumber() {
+        return this.sumber;
+    }
+
+    // setter methods
+    public void setTotalPemasukan(double totalPemasukan) {
+        this.totalPemasukan = totalPemasukan;
+    }
+
+    public void setSumber(String sumber) {
+        this.sumber = sumber;
+    }
+
+    // additional methods
+    public double addPemasukan() {
+        this.totalAnggaran += this.totalPemasukan;
+        return this.totalAnggaran;
+    }
+}
